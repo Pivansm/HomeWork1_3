e36100188a97f277c252183d11ec26c88ee95279
Pivansm <pivansm@outlook.com>
2021-05-09 11:42:27 +0300

    "Hello Gradle V1.0"

---
+++ src/main/java/ru/sberbank/restart/HelloWorld.java
@@ -0,0 +1,13 @@
+package ru.sberbank.restart;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Текст класса HelloWorld Gradle
+ * @author Ivan
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.upperCase("hello world!" ));
+    }
+}
